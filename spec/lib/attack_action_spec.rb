9736d0d3908007a079da11c97254eaf66b0e52ca
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 10:04:46 -0600

    Attack actions sends skill_check message to dicepool

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe AttackAction do
-  let(:hero) { double('hero') }
+  let(:hero) { double('hero', strength: 3) }
+  let(:dicepool) { double('dicepool') }
+  let(:monster) { double('monster', toughness: 2) }
 
-  subject { AttackAction.new hero }
+  subject { AttackAction.new hero, dicepool }
 
   it 'responds to activate message' do
     expect(subject).to respond_to(:activate)
@@ -11,5 +13,12 @@ describe AttackAction do
 
   it 'has an owner' do
     expect(subject.owner).to eq(hero)
+  end
+
+  describe '#activate' do
+    it 'makes strength check against target toughbess' do
+      dicepool.should_receive(:skill_check).with(hero.strength, monster.toughness)
+      subject.activate(monster)
+    end
   end
 end
