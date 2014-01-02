fa7863da4d23a0664d41aea969e30af797578880
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 12:16:31 -0600

    Add Monster attributes

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,3 +1,8 @@
 class Monster
+  attr_reader :toughness, :damage
 
+  def initialize(attr = {})
+    @toughness = attr.fetch(:toughness, 2)
+    @damage = attr.fetch(:damage, 2)
+  end
 end
--- spec/lib/monster_spec.rb
+++ spec/lib/monster_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Monster do
+  let(:monster) { Monster.new toughness: 2, damage: 4 }
 
+   it 'has toughness' do
+     expect(monster.toughness).to eq 2
+   end
+
+   it 'has damage' do
+     expect(monster.damage).to eq 4
+   end
 end
