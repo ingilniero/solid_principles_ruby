358a333fd8267f987b2d7036f645e5b3680a03a2
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 11:02:24 -0600

    Add skill_check happy path

--- spec/lib/dicepool_spec.rb
+++ spec/lib/dicepool_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Dicepool do
+  describe '#skill_check' do
+    context 'when number of successes is more than difficulty' do
+      before do
+        subject.stub(:roll_die).and_return(5)
+      end
 
+      it { expect(subject.skill_check(3,2)).to be_true }
+    end
+  end
 end
