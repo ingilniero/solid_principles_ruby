17b3c46d5b009e5c3156963332a917aaf57bdb84
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 09:59:20 -0600

    Attack action has an owner

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe AttackAction do
+  let(:hero) { double('hero') }
+
+  subject { AttackAction.new hero }
+
   it 'responds to activate message' do
     expect(subject).to respond_to(:activate)
+  end
+
+  it 'has an owner' do
+    expect(subject.owner).to eq(hero)
   end
 end
