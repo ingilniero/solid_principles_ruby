02c78af9988dd3f865c1ac2b15f9d7de0a2880f4
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 12:39:02 -0600

    Add monster notice attribute

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,9 +1,10 @@
 class Monster
-  attr_reader :toughness, :damage
+  attr_reader :toughness, :damage, :notice
 
   def initialize(attr = {})
     @toughness = attr.fetch(:toughness, 2)
     @damage = attr.fetch(:damage, 2)
+    @notice = attr.fetch(:notice, 2)
     @dead = false
   end
 
--- spec/lib/monster_spec.rb
+++ spec/lib/monster_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Monster do
-  let(:monster) { Monster.new toughness: 2, damage: 4 }
+  let(:monster) { Monster.new toughness: 2, damage: 4, notice: 2 }
 
    it 'has toughness' do
      expect(monster.toughness).to eq 2
@@ -9,6 +9,10 @@ describe Monster do
 
    it 'has damage' do
      expect(monster.damage).to eq 4
+   end
+
+   it 'has notice' do
+     expect(monster.notice).to eq 2
    end
 
    describe '#dead?' do
