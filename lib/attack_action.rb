8b6b371ba5d3ad0999b4f1745c98a361c3b8085b
Gilberto Villa <ingilniero@gmail.com>
2014-01-02 09:57:05 -0600

    AttackAction responds to activate

--- lib/attack_action.rb
+++ lib/attack_action.rb
@@ -1,3 +1,4 @@
 class AttackAction
-
+  def activate(target)
+  end
 end
--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe AttackAction do
+  it 'responds to activate message' do
+    expect(subject).to respond_to(:activate)
+  end
 end
